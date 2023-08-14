606fb1517e35870bdd1b28a96609d5c96a555693
Rishi <l.rishissahu@gmail.com>
2023-08-14 18:13:08 +0530

    Search bar issues Fixed

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import "./Header.css"
-import { Link} from "react-router-dom"
+import { Link } from "react-router-dom"
 import SearchBar from '../searchBar/SearchBar'
 import { useAuth0 } from "@auth0/auth0-react";
 
 const Header = () => {
   const { loginWithRedirect, isAuthenticated, logout, user } = useAuth0();
-  
+
   return (
     <div className='header'>
-        <Link  exact to="/"><img className='header__icon' src="cine.png"/></Link>
+      <Link exact to="/"><img className='header__icon' src="cine.png" /></Link>
       <div className='headerLeft'>
-        <Link  exact to="/movies/popular" style={{ textDecoration: 'none' }}><span>Popular</span></Link>
+        <Link exact to="/movies/popular" style={{ textDecoration: 'none' }}><span>Popular</span></Link>
         <Link exact to="/movies/top_rated" style={{ textDecoration: 'none' }}><span>Top Rated</span></Link>
         <Link exact to="/movies/upcoming" style={{ textDecoration: 'none' }}><span>UpComming</span></Link>
 
@@ -23,18 +23,18 @@ const Header = () => {
         {
           isAuthenticated ? (
             <div className='menu-bar'>
-            <ul>
-            <li><a href="#">{user.name}<i className="fas fa-caret-down"></i></a>
-              <div className="dropdown-menu">
-                <ul>
-                  <li><a href="#">Your Watchlist</a></li>
-                  <li><a href="#"></a><button className='b' onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
-                    Sign Out
-                  </button></li>
-                </ul>
-              </div>
-            </li>
-            </ul>
+              <ul>
+                <li><a href="#">{user.name}<i className="fas fa-caret-down"></i></a>
+                  <div className="dropdown-menu">
+                    <ul>
+                      <li><a href="#">Your Watchlist</a></li>
+                      <li><a href="#"></a><button className='b' onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
+                        Sign Out
+                      </button></li>
+                    </ul>
+                  </div>
+                </li>
+              </ul>
             </div>
 
           ) : (
--- src/pages/movieDetail/Movie.js
+++ src/pages/movieDetail/Movie.js
@@ -1,23 +1,23 @@
-import React ,{useEffect,useState}from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Movie.css"
-import {useParams} from "react-router-dom"
+import { useParams } from "react-router-dom"
 
 const Movie = () => {
-    const [currentMovieDetail,setMovie] = useState()
-    const {id} =useParams()
+    const [currentMovieDetail, setMovie] = useState()
+    const { id } = useParams()
 
     useEffect(() => {
         getData()
-        window.scrollTo(0,0)
-    }, [])
-    
-    const getData=()=>{
+        window.scrollTo(0, 0)
+    }, [id])
+
+    const getData = () => {
         fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US`)
-        .then(res=>res.json())
-        .then(data=>setMovie(data))
+            .then(res => res.json())
+            .then(data => setMovie(data))
     }
-  return (
-    <div className="movie">
+    return (
+        <div className="movie">
             <div className="movie__intro">
                 <img className="movie__backdrop" src={`https://image.tmdb.org/t/p/original${currentMovieDetail ? currentMovieDetail.backdrop_path : ""}`} />
             </div>
@@ -32,20 +32,20 @@ const Movie = () => {
                         <div className="movie__name">{currentMovieDetail ? currentMovieDetail.original_title : ""}</div>
                         <div className="movie__tagline">{currentMovieDetail ? currentMovieDetail.tagline : ""}</div>
                         <div className="movie__rating">
-                            {currentMovieDetail ? currentMovieDetail.vote_average: ""} 
+                            {currentMovieDetail ? currentMovieDetail.vote_average : ""}
                             <span className="movie__voteCount">{currentMovieDetail ? "(" + currentMovieDetail.vote_count + ") votes" : ""}</span>
-                        </div>  
+                        </div>
                         <div className="movie__runtime">{currentMovieDetail ? currentMovieDetail.runtime + " mins" : ""}</div>
                         <div className="movie__releaseDate">{currentMovieDetail ? "Release date: " + currentMovieDetail.release_date : ""}</div>
                         <div className="movie__genres">
                             {
                                 currentMovieDetail && currentMovieDetail.genres
-                                ? 
-                                currentMovieDetail.genres.map(genre => (
-                                    <><span className="movie__genre" id={genre.id}>{genre.name}</span></>
-                                )) 
-                                : 
-                                ""
+                                    ?
+                                    currentMovieDetail.genres.map(genre => (
+                                        <><span className="movie__genre" id={genre.id}>{genre.name}</span></>
+                                    ))
+                                    :
+                                    ""
                             }
                         </div>
                     </div>
@@ -53,14 +53,14 @@ const Movie = () => {
                         <div className="synopsisText">Synopsis</div>
                         <div>{currentMovieDetail ? currentMovieDetail.overview : ""}</div>
                         <div className="movie__links">
-                <div className="movie__heading">Useful Links :</div>
-                {
-                    currentMovieDetail && currentMovieDetail.homepage && <a href={currentMovieDetail.homepage} target="_blank" style={{textDecoration: "none"}}><p><span className="movie__homeButton movie__Button">Homepage <i className="newTab fas fa-external-link-alt"></i></span></p></a>
-                }
-                {
-                    currentMovieDetail && currentMovieDetail.imdb_id && <a href={"https://www.imdb.com/title/" + currentMovieDetail.imdb_id} target="_blank" style={{textDecoration: "none"}}><p><span className="movie__imdbButton movie__Button">IMDb<i className="newTab fas fa-external-link-alt"></i></span></p></a>
-                }
-            </div>
+                            <div className="movie__heading">Useful Links :</div>
+                            {
+                                currentMovieDetail && currentMovieDetail.homepage && <a href={currentMovieDetail.homepage} target="_blank" style={{ textDecoration: "none" }}><p><span className="movie__homeButton movie__Button">Homepage <i className="newTab fas fa-external-link-alt"></i></span></p></a>
+                            }
+                            {
+                                currentMovieDetail && currentMovieDetail.imdb_id && <a href={"https://www.imdb.com/title/" + currentMovieDetail.imdb_id} target="_blank" style={{ textDecoration: "none" }}><p><span className="movie__imdbButton movie__Button">IMDb<i className="newTab fas fa-external-link-alt"></i></span></p></a>
+                            }
+                        </div>
                     </div>
                 </div>
             </div>
